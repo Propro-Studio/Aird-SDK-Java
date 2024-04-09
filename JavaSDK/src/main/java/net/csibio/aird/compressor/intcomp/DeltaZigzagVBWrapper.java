3a6ee4b33e87c80ba03b8ac61267e018585a45ee
lms <lms19880914@qq.com>
2024-04-09 20:05:39 +0800

    新增DeltaZigzagVariabeByte压缩器，用于更好的压缩离子淌度数据 版本号增至2.4.3

--- JavaSDK/src/main/java/net/csibio/aird/compressor/intcomp/DeltaZigzagVBWrapper.java
+++ JavaSDK/src/main/java/net/csibio/aird/compressor/intcomp/DeltaZigzagVBWrapper.java
@@ -5,7 +5,7 @@ import net.csibio.aird.enums.IntCompType;
 
 import java.util.Arrays;
 
-public class DeltaZigzagVBWrapper implements IntComp{
+public class DeltaZigzagVBWrapper implements IntComp {
     @Override
     public String getName() {
         return IntCompType.DZVB.getName();
@@ -14,22 +14,22 @@ public class DeltaZigzagVBWrapper implements IntComp{
     @Override
     public int[] encode(int[] uncompressed) {
         IntegerCODEC codec = new DeltaZigzagVariableByte();
-        int[] compressed = new int[uncompressed.length + uncompressed.length/100 + 1024];// could need more
+        int[] compressed = new int[uncompressed.length + uncompressed.length / 100 + 1024];// could need more
         IntWrapper outputoffset = new IntWrapper(1);
         codec.compress(uncompressed, new IntWrapper(0), uncompressed.length, compressed, outputoffset);
-        compressed = Arrays.copyOf(compressed,outputoffset.intValue());
+        compressed = Arrays.copyOf(compressed, outputoffset.intValue());
         compressed[0] = uncompressed.length;
         return compressed;
     }
 
     @Override
     public int[] decode(int[] compressed) {
-        if (compressed.length == 0){
+        if (compressed.length == 0) {
             return new int[0];
         }
         IntegerCODEC codec = new DeltaZigzagVariableByte();
         int[] decompressed = new int[compressed[0]];
-        codec.uncompress(compressed, new IntWrapper(1), compressed.length-1, decompressed, new IntWrapper(0));
+        codec.uncompress(compressed, new IntWrapper(1), compressed.length - 1, decompressed, new IntWrapper(0));
         return decompressed;
     }
 }
